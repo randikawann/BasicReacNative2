24cf169fe9739e84a92f2b1e4c0d01135e97da2b
randikawan <randikawann@gmail.com>
2019-02-14 13:12:46 +0530

    complete mooving between screen but not wrok ios

--- App.js
+++ App.js
@@ -1,40 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+
+        {/* <Button
+          title="Go to Home... again"
+          onPress={() => this.props.navigation.navigate('Home')}
+        /> */}
+        //add more button
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.push('Details')}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+        
+      </View>
+    );
+  }
+}
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Home Screen</Text>
+        {/* added new button to home screen */}
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+
       </View>
     );
   }
 }
 
 const AppNavigator = createStackNavigator({
+ //no need
+  /*
   Home: {
     screen: HomeScreen
   }
+  */
+      //no need above things 
+//  Home: HomeScreen
+      //multiple class added
+  Home: HomeScreen,
+  Details: DetailsScreen
+},
+{
+  initialRouteName: "Home"
 });
 
-// export default createAppContainer(AppNavigator);
 const AppContainer = createAppContainer(AppNavigator);
 
 type Props = {};
 
 
-// class App extends Component<Props> {
 export default class App extends React.Component {
   render() {
     return ( <AppContainer />
